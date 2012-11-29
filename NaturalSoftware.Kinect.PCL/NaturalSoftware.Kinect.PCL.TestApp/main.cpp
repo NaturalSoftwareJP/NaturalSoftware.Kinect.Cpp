b4c75f852c4686bd7054dea37e0f459ad04dc49b
kaorun <kaorun55@gmail.com>
2012-11-29 17:37:02 +0900

    不要なヘッダーを削除

--- NaturalSoftware.Kinect.PCL/NaturalSoftware.Kinect.PCL.TestApp/main.cpp
+++ NaturalSoftware.Kinect.PCL/NaturalSoftware.Kinect.PCL.TestApp/main.cpp
@@ -1,16 +1,7 @@
 #include <Windows.h>
 #include <NaturalSoftware.Kinect.PCL\KinectGrabber.h>
 
-//#include <pcl/ModelCoefficients.h>
-//#include <pcl/point_types.h>
-//#include <pcl/sample_consensus/method_types.h>
-//#include <pcl/sample_consensus/model_types.h>
-//#include <pcl/segmentation/sac_segmentation.h>
 #include <pcl/visualization/cloud_viewer.h>
-
-//#include <pcl/filters/extract_indices.h>
-//#include <pcl/segmentation/extract_clusters.h>
-
 
 void main()
 {
